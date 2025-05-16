f2b934156db7dbcd158adfae488ebee428987509
Kale-Sudhir <sudhir04kale@gmail.com>
2025-05-16 12:01:27 +0530

    Fix import paths for images in Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,8 @@
 import React from "react";
-import Sudhir_portfolio from "./images/sudhir_kale_portfolio.png";
-import Ks_elects from "./images/KS_Elects.png";
-import Youtube from "./images/Youtube_Clone.png";
-import cloudy  from "./images/Cloudy_Area.png";
+import Sudhir_portfolio from "../Images/sudhir_kale_portfolio.png";
+import Ks_elects from "../Images/KS_Elects.png";
+import Youtube from "../Images/Youtube_Clone.png";
+import cloudy  from "../Images/Cloudy_Area.png";
 
 const Projects = ({dta}) => {
   return (
